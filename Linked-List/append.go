d96d9cbdfe81a7a9e5eb3b469de5c822316a7294
2dvoid <2dvoid@mail.com>
2026-07-17 01:21:34 +0600

    Linked-List: implemented Append function

--- Linked-List/append.go
+++ Linked-List/append.go
@@ -1,5 +1,21 @@
 package main
 
-func (l *LinkedList) Append(val int){
+func (l *LinkedList) Append(val int) {
+	// Create a new Node
+	newNode := &Node{
+		data: val,
+	}
+	// Check if the provided list is empty or not
+	if l.head == nil {
+		// If Empty then attach the new node the the head directly
+		l.head = newNode
+		l.tail = newNode
+	} else {
+		// If list not empty
+		// Attach the new node the the tail node (last node)
+		l.tail.link = newNode
+		// Update the global tail
+		l.tail = newNode
+	}
 
 }
